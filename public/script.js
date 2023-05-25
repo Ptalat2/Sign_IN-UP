deed1501a3e35d9983e4e8821ff1ea867e4c1ad6
Ptalat2 <ptalat2@uic.edu>
2023-05-25 01:28:33 -0500

    error check #1

---
+++ public/script.js
@@ -0,0 +1,9 @@
+function handleServerError() {
+    // Code to handle the server error signal
+    // For example, you can modify the DOM or perform any desired operations
+    if(count> 0 ) {
+      alert("wrong password");
+    }
+  }
+
+ 
--- src/index.js
+++ src/index.js
@@ -45,22 +45,27 @@ app.post("/signup", async (req, res) => {
       // Handle the error appropriately
     }
   });
+  let count =0;
 
   app.post("/login", async (req, res) => {
-   
-   
-    try {
-        const check = await collection.findOne ({name:req.body.name})     
-        if(check.password === req.body.password) {
-            res.render("home");
-        } else {
-            res.send("wrong password")
-        }
-        
-    } catch (error) {
-        res.send("wrong details")
-        console.error(error);
-      // Handle the error appropriately
+  try {
+    const check = await collection.findOne({ name: req.body.name });
+    if (check.password === req.body.password) {
+      res.render("home");
+    } else {
+        res.render('login', { errorResponse: { message: 'Wrong password', count:count } });
+        ++count;
     }
-  });
+  } catch (error) {
+    res.render('login', { errorResponse: { message: 'Wrong details' ,  count:count } });
+    console.error(error);
+    ++count;
+    // Handle the error appropriately
+  }
+});
+
+  
+  
+  
+  
 
